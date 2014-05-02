8f180e98a32510f5d056edf1694580909cefd344
Diego Reiriz <diegoreiriz@gmail.com>
2014-05-02 23:58:53 +0200

    no message

--- main.c
+++ main.c
@@ -12,34 +12,38 @@
  * 
  */
 int main(int argc, char** argv) {
-    
+
     char opt;
     short mode; //variable que indica en que modo se esta traballando
-    
-    do{
+
+    do {
         printf("\nModo de Operacion");
         printf("\n===================");
         printf("\n1)Monedas Infinitas");
         printf("\n2)Monedas Limitadas");
         printf("\n0)Salir");
         printf("\nOpcion: ");
-        scanf("%c",&opt);//corregir fallo no menu
-        
-        switch(opt){
+        scanf(" %c", &opt); //corregir fallo no menu
+        getchar();
+
+        switch (opt) {
             case '1'://Monedas Infinitas
-                
+                solicitarMonedas();
                 break;
             case '2'://Monedas Limitadas
-                
+                solicitarMonedas();
+                break;
+            case '0':
+                printf("\nFin del Prograna");
                 break;
             default:
                 printf("\nOpcion Erronea");
                 break;
         }
-        
-    }while(opt != '0');
-    
-    return(EXIT_SUCCESS);
+
+    } while (opt != '0');
+
+    return (EXIT_SUCCESS);
 }
 
 /** SOLICITAR MONEDAS
@@ -49,9 +53,36 @@ int main(int argc, char** argv) {
  * 
  * @return int - numero de monedas
  */
-int solicitarMonedas(){
+int solicitarMonedas() {
     int monedas;
     printf("\nIntroduzca o numero de monedas do que quere cambio: ");
-    scanf("%d",&monedas);
+    scanf(" %d", &monedas);
     return monedas;
+}
+
+/** cambio
+ * algoritmo voraz para dar o cambio
+ * 
+ * @param int - cantidad a devolver
+ * @param vectorP - valor das monedas
+ * @param vectorP - vector que almacena a cantidad de monedas que se devolve de cada tipo
+ * 
+ * @return Devolve un 1 si se dou a volta correctamente ou un 0
+ * si se devolve un vector vacio.
+ * 
+ */
+int cambio(int x, int valor[n], int solucion[n]) {
+    for (int i = 0; i < n; i++) solucion[i] = 0;
+    int i = 0, suma = 0;
+    while (suma < x && i < n)
+        if (suma + valor[i] <= x) {
+            solucion[i]++;
+            suma += valor[i];
+        } else
+            i++;
+    if (suma == x) return 1;
+    else {
+        for (int i = 0; i < n; i++) solucion[i] = 0;
+        return 0;
+    }
 }
