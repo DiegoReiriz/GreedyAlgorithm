35f73217ce9d9f9ab98a49938497dca4ebc6c33f
Diego Reiriz <diegoreiriz@gmail.com>
2014-05-09 19:55:51 +0200

    no message

--- main.c
+++ main.c
@@ -14,33 +14,27 @@ void imprimirVector(vectorP vec);
 int cambioInf(int x, vectorP valor, vectorP *solucion);
 void escribirMonedas();
 void leerMonedas();
-    
+void carasEuros(vectorP *eur);
+void carasDolar(vectorP *dol);
+void carasYen(vectorP *y);
 
 int main(int argc, char** argv) {
-escribirMonedas();
-leerMonedas();
+    //escribirMonedas();
+    //leerMonedas();
 
-    char opt,inf;
-    
-    char monedas[3][6]={"euro","dolar","yen"};
+    char opt, inf;
+
+    char monedas[3][6] = {"euro", "dolar", "yen"};
     char monActual;
-    
+
     short mode; //variable que indica en que modo se esta traballando
+
 
     /*
      * Asignacion temporal de monedas
      */
-    vectorP euros,stockEuros,dolares,sotckDolares,yen,stockYens, solucion;
-    crear(&euros, 8);
-    crear(&solucion, 8);
-    asignar(&euros, 0, 200);
-    asignar(&euros, 1, 100);
-    asignar(&euros, 2, 50);
-    asignar(&euros, 3, 20);
-    asignar(&euros, 4, 10);
-    asignar(&euros, 5, 5);
-    asignar(&euros, 6, 2);
-    asignar(&euros, 7, 1);
+    vectorP moneda = NULL, solucion = NULL;
+
     do {
         printf("\nModo de Operacion");
         printf("\n===================");
@@ -51,17 +45,17 @@ leerMonedas();
         scanf(" %c", &opt); //corregir fallo no menu
         getchar();
 
-        inf=0;//por defecto non se activa o modo infinito salvo que se seleccione no menu
+        inf = 0; //por defecto non se activa o modo infinito salvo que se seleccione no menu
         switch (opt) {
             case '1'://Monedas Infinitas
-                inf=1;
-                
+                inf = 1;
+
             case '2'://Monedas Limitadas
-                
-                if(!inf){
-                    
+
+                if (!inf) {
+
                 }
-                
+
                 do {
                     printf("\nSeleccionar Tipo de Moneda");
                     printf("\n==========================");
@@ -73,44 +67,58 @@ leerMonedas();
                     scanf(" %c", &opt); //corregir fallo no menu
                     getchar();
 
-                    monActual=( sizeof(monedas) / sizeof(char) ) / (sizeof(monedas[0]) / sizeof(char) );//variable que gestiona que elemento do array que maneja os tipos de mona estamos usando
-                    
+                    monActual = (sizeof (monedas) / sizeof (char)) / (sizeof (monedas[0]) / sizeof (char)); //variable que gestiona que elemento do array que maneja os tipos de mona estamos usando
+
+                    if (moneda != NULL)
+                        liberar(&moneda);
+
+                    if (solucion != NULL)
+                        liberar(&solucion);
+
                     switch (opt) {
-                        case '1':
-                            monActual--;
-                            
-                        case '2':
-                            monActual--;
-                            
-                        case '3':
-                            monActual--;
-                            
-                            printf("\nMoneda Seleccionada: %s \n",monedas[monActual]);
-                            
-                            cambioInf(solicitarMonedas(), euros, &solucion);
-                            imprimirVector(solucion);
+                        case '1'://euro
+                            crear(&moneda, 8);
+                            carasEuros(&moneda);
+                            crear(&solucion, 8);
                             break;
+                        case '2'://dolar
+                            crear(&moneda, 4);
+                            carasDolar(&moneda);
+                            crear(&solucion, 4);
+                            break;
+                        case '3'://yen
+                            crear(&moneda, 6);
+                            carasYen(&moneda);
+                            crear(&solucion, 6);
+                            break;
+
+                            //printf("\nMoneda Seleccionada: %s \n",monedas[monActual-strtol(opt,NULL,10)]);
+
                         default:
 
                             break;
                     }
 
+                    cambioInf(solicitarMonedas(), moneda, &solucion);
+                    imprimirVector(moneda);
+                    imprimirVector(solucion);
+
                 } while (opt != '0');
-                
+
                 opt = ' ';
-                
+
                 break;
             case '0':
-                
+
                 break;
             default:
                 printf("\nOpcion Erronea");
                 break;
         }
-        
+
     } while (opt != '0');
 
-    liberar(&euros);
+    liberar(&moneda);
     return (EXIT_SUCCESS);
 }
 
@@ -179,50 +187,86 @@ void imprimirVector(vectorP vec) {
     printf("\n");
     for (i = 0; i < len; i++) {
         recuperar(vec, i, &aux);
-        printf("%d \t",aux);
+        printf("%d \t", aux);
     }
 }
 
-void escribirMonedas(){
-    FILE *fp;
-    fp=fopen("monedas.txt","w");
-    
-    fprintf(fp,"euro 8");
-    fprintf(fp,"\n200 0");
-    fprintf(fp,"\n100 0");
-    fprintf(fp,"\n50 0");
-    fprintf(fp,"\n25 0");
-    fprintf(fp,"\n10 0");
-    fprintf(fp,"\n5 0");
-    fprintf(fp,"\n2 0");
-    fprintf(fp,"\n1 0");
-    fprintf(fp,"\n");
-    fprintf(fp,"\ndolar 4");
-    fprintf(fp,"\n25 0");
-    fprintf(fp,"\n10 0");
-    fprintf(fp,"\n5 0");
-    fprintf(fp,"\n1 0");
-    fprintf(fp,"\n");
-    fprintf(fp,"\nyen 6");
-    fprintf(fp,"\n500 0");
-    fprintf(fp,"\n100 0");
-    fprintf(fp,"\n50 0");
-    fprintf(fp,"\n10 0");
-    fprintf(fp,"\n5 0");
-    fprintf(fp,"\n1 0");
-    fclose(fp);
-    
+void carasEuros(vectorP *eur) {
+    asignar(eur, 0, 200);
+    asignar(eur, 1, 100);
+    asignar(eur, 2, 50);
+    asignar(eur, 3, 20);
+    asignar(eur, 4, 10);
+    asignar(eur, 5, 5);
+    asignar(eur, 6, 2);
+    asignar(eur, 7, 1);
 }
-void leerMonedas(){
+
+void carasDolar(vectorP *dol) {
+    asignar(dol, 0, 25);
+    asignar(dol, 1, 10);
+    asignar(dol, 2, 5);
+    asignar(dol, 3, 1);
+}
+
+void carasYen(vectorP *y) {
+    asignar(y, 0, 500);
+    asignar(y, 1, 100);
+    asignar(y, 2, 50);
+    asignar(y, 3, 10);
+    asignar(y, 4, 5);
+    asignar(y, 5, 1);
+}
+
+void escribirMonedas() {
     FILE *fp;
-    int aux,aux3;
-    char aux2[10];
-    
-    fp=fopen("monedas.txt","r");
-    
-    do{
-        aux=fscanf(fp,"%s %d",&aux2,&aux3);
-    }while(aux!= EOF);
-    
+    fp = fopen("monedas.txt", "w");
+
+    fprintf(fp, "euro 8");
+    fprintf(fp, "\n200 0");
+    fprintf(fp, "\n100 0");
+    fprintf(fp, "\n50 0");
+    fprintf(fp, "\n25 0");
+    fprintf(fp, "\n10 0");
+    fprintf(fp, "\n5 0");
+    fprintf(fp, "\n2 0");
+    fprintf(fp, "\n1 0");
+    fprintf(fp, "\n");
+    fprintf(fp, "\ndolar 4");
+    fprintf(fp, "\n25 0");
+    fprintf(fp, "\n10 0");
+    fprintf(fp, "\n5 0");
+    fprintf(fp, "\n1 0");
+    fprintf(fp, "\n");
+    fprintf(fp, "\nyen 6");
+    fprintf(fp, "\n500 0");
+    fprintf(fp, "\n100 0");
+    fprintf(fp, "\n50 0");
+    fprintf(fp, "\n10 0");
+    fprintf(fp, "\n5 0");
+    fprintf(fp, "\n1 0");
+    fclose(fp);
+
+}
+
+void leerMonedas() {
+    FILE *fp;
+    vectorP vec, stock;
+    int aux, caras, i;
+    char nombre[10];
+
+    fp = fopen("monedas.txt", "r");
+
+    do {
+        aux = fscanf(fp, "%s %d", &nombre, &caras);
+
+
+
+        for (i = 0; i < caras; i++) {
+
+        }
+
+    } while (aux != EOF);
+
     fclose(fp);
 }
